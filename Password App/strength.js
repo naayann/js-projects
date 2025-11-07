378618fbbaaf308646d40b860b71a95c74895037
Nayan Sarania <nayan.sarania.work@gmail.com>
2025-11-07 18:19:25 +0530

    feat: add password show and hide button

--- Password App/strength.js
+++ Password App/strength.js
@@ -2,6 +2,10 @@ let pass = document.getElementById("password");
 let msg = document.getElementById("message");
 let str = document.getElementById("strength");
 
+let eyeIcon = document.getElementById("eyeIcon");
+let eyeopen = '<i class="ri-eye-line"></i>';
+let eyeclosed = '<i class="ri-eye-off-line">'
+
 pass.addEventListener("input", () => {
     if(pass.value.length > 0) {
         msg.style.display = "block";
@@ -21,5 +25,15 @@ pass.addEventListener("input", () => {
         str.innerHTML = "strong"
         pass.style.borderColor = "#26d730"
         msg.style.color = "#26d730"
+    }
+})
+
+eyeIcon.addEventListener("click", () => {
+    if(pass.type == "password") {
+        pass.type = "text";
+        eyeIcon.innerHTML = eyeopen;
+    } else {
+        pass.type = "password";
+        eyeIcon.innerHTML = eyeclosed;
     }
 })
