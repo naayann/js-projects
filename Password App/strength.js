451f2fd9157f52ffe00e32e919af693b90da7d2a
Nayan Sarania <nayan.sarania.work@gmail.com>
2025-11-07 14:26:25 +0530

    add home page with password strength feature

---
+++ Password App/strength.js
@@ -0,0 +1,25 @@
+let pass = document.getElementById("password");
+let msg = document.getElementById("message");
+let str = document.getElementById("strength");
+
+pass.addEventListener("input", () => {
+    if(pass.value.length > 0) {
+        msg.style.display = "block";
+    } else {
+        msg.style.display = "none"
+    }
+
+    if(pass.value.length < 4) {
+        str.innerHTML = "weak"
+        pass.style.borderColor = "red"
+        msg.style.color = "red"
+    } else if(pass.value.length < 8) {
+        str.innerHTML = "moderate"
+        pass.style.borderColor = "#ffe100ff"
+        msg.style.color = "#ffe100ff"
+    } else if(pass.value.length >= 10) {
+        str.innerHTML = "strong"
+        pass.style.borderColor = "#26d730"
+        msg.style.color = "#26d730"
+    }
+})
