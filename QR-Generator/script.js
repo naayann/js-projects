6350796db90b86dedd4a1e22a8e12feeccb455cc
Nayan Sarania <nayan.sarania.work@gmail.com>
2025-11-16 13:12:20 +0530

    Add functional download QR button

--- QR-Generator/script.js
+++ QR-Generator/script.js
@@ -1,12 +1,17 @@
 let imgBox = document.getElementById("imgBox")
 let qrImage = document.getElementById("qrImage")
 const qrText = document.getElementById("qrText")
+const generateBtn = document.getElementById("generateBtn")
+const downloadBtn = document.getElementById("downloadBtn")
 
 function generateQR() {
     if(qrText.value.length > 0) {
         qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
 
         imgBox.classList.add("show-img");
+
+        generateBtn.style.display = "none"
+        downloadBtn.style.display = "block"
     } else {
         qrText.classList.add('error');
         setTimeout(() => {
@@ -14,3 +19,10 @@ function generateQR() {
         },1000);
     }
 }
+
+downloadBtn.addEventListener("click", function () {
+    const link = document.createElement("a");
+    link.href = qrImage.src;
+    link.download = "theQRcode";
+    link.click();
+});
