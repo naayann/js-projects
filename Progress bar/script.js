aa15db02dcc38ae2a7aa3d0a9af911b1981dd5d1
Nayan Sarania <nayan.sarania.work@gmail.com>
2025-11-18 11:17:41 +0530

    shorten js using html attributes and forEach loop

--- Progress bar/script.js
+++ Progress bar/script.js
@@ -2,11 +2,19 @@ const ten = document.getElementById('ten')
 const thirty = document.getElementById('thirty')
 const sixty = document.getElementById('sixty')
 const ninty = document.getElementById('ninty')
+const circle = document.getElementById('circle')
+
+document.querySelectorAll('.btn').forEach(btn => {
+  btn.addEventListener('click', () => {
+    value = Number(btn.dataset.value);
+  });
+});
+
 
 let number = document.getElementById("number");
 let counter = 0;
 setInterval(() => {
-    if(counter == 65) {
+    if(counter == value) {
         clearInterval();
     } else {
         counter += 1;
