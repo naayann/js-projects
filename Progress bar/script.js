5d09a5d803e5cbb8c7d5fea9e1928c135bb5afd0
Nayan Sarania <nayan.sarania.work@gmail.com>
2025-11-18 11:35:31 +0530

    make the progress bar interactive

--- Progress bar/script.js
+++ Progress bar/script.js
@@ -4,11 +4,33 @@ const sixty = document.getElementById('sixty')
 const ninty = document.getElementById('ninty')
 const circle = document.getElementById('circle')
 
-document.querySelectorAll('.btn').forEach(btn => {
-  btn.addEventListener('click', () => {
-    value = Number(btn.dataset.value);
-  });
-});
+// document.querySelectorAll('.btn').forEach(btn => {
+//   btn.addEventListener('click', () => {
+//     value = Number(btn.dataset.value);
+//     document.documentElement.style.setProperty("--dash", "300");
+//   });
+// });
+
+ten.addEventListener('click', () => {
+  value = 10;
+  document.documentElement.style.setProperty("--dash", "400");
+})
+
+thirty.addEventListener('click', () => {
+  value = 30;
+  document.documentElement.style.setProperty("--dash", "300");
+})
+
+sixty.addEventListener('click', () => {
+  value = 60;
+  document.documentElement.style.setProperty("--dash", "200");
+})
+
+ninty.addEventListener('click', () => {
+  value = 90;
+  document.documentElement.style.setProperty("--dash", "100");
+})
+
 
 
 let number = document.getElementById("number");
